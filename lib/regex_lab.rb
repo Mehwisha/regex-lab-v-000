5914b003988a3be3b7c611282c65ae9c109c240e
Mehwish Aziz <mehwisha@msn.com>
2018-04-28 18:22:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\b[aeiou]\w*/i)
+  return true
+elsif word
+  false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+!!text.match(/^[A-Z].*\W$/)
 
 end
 
-def valid_phone_number?(phone)
 
+def valid_phone_number?(phone)
+if  phone.scan(/^(\d{3})(\d{3})(\d{4})$/) || phone
+  return true
+elsif !phone.scan(/^(\d{3})(\d{3})(\d{4})$/)
+    false
+end
 end
