2f426f22a0fbf850f4537c0df86f1a10833a0f1a
Mehwish Aziz <mehwisha@msn.com>
2018-04-28 18:48:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ end
 def valid_phone_number?(phone)
   if phone.scan(/^(\d{3})(\d{3})(\d{4})$/)
     return true
-  elsif !phone.scan(/^(\d{3})(\d{3})(\d{4})$/)
+  elsif phone.scan(/^(\d{3})(\d{3})(\d{4})$/) == nil
     return false
 end
 end
